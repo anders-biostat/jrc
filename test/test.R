c63a85de817f61fd53f602b2332e45db4cbdbd8a
Unknown <svetlana.ovchinnikova@helsinki.fi>
2018-05-03 16:55:29 +0200

    Test script is updated

--- test/test.R
+++ test/test.R
@@ -6,13 +6,13 @@ k <- 0
 openPage()
 sendCommand(str_c("button = document.createElement('input');",
               "button.type = 'button';",
-              "button.addEventListener('click', function() {ws.send('k <<- k + 1')});", 
+              "button.addEventListener('click', function() {jrc.sendCommand('k <<- k + 1')});", 
               "button.value = '+1';",
               "document.body.appendChild(button);", collapse = "\n"))
 closePage()
 
 #load linked charts from a local file
-openPage(localDirectory = "~/Work/Git/linked-charts/")
+openPage(rootDirectory = "~/Work/Git/linked-charts/")
 x <- seq(0, 5, length.out = 100)
 y <- sin(x) + rnorm(n = 100, sd = 0.2)
 
@@ -39,40 +39,7 @@ sendCommand(str_c("sc = lc.scatter()",
               ".x(function(k) {return x[k]})",
               ".y(function(k) {return y[k]})",
               ".colour(function(k) {return k == selPoint ? 'red' : 'black';})",
-              ".on_click(function(k) {selPoint = k; ws.send('selPoint <<- ' + k); ws.send('print(selPoint)'); sc.update()})", 
+              ".on_click(function(k) {selPoint = k; jrc.sendCommand('selPoint <<- ' + k); jrc.sendCommand('print(selPoint)'); sc.update()})", 
               ".place()", collapse = "\n"))
 
 closePage()
-
-
-
-openPage(useViewer = F)
-x <- seq(0, 5, length.out = 100)
-y <- sin(x) + rnorm(n = 100, sd = 0.2)
-sendData("x", x)
-sendData("y", y)
-
-selPoint <- -1
-
-#note that external files can't be loaded from the viewer
-sendCommand(str_c("script = document.createElement('script');", 
-              "script.src = 'https://kloivenn.github.io/linked-charts/lib/linked-charts.min.js';", 
-              "document.head.appendChild(script);", collapse = "\n"))
-
-sendCommand(str_c("link = document.createElement('link');", 
-              "link.rel = 'stylesheet';", 
-              "link.href = 'https://kloivenn.github.io/linked-charts/lib/linked-charts.css';", 
-              "document.head.appendChild(link);", collapse = "\n"))
-
-sendCommand("selPoint = -1")
-sendCommand(str_c("var sc = lc.scatter()", 
-              ".x(function(k) {return x[k]})",
-              ".y(function(k) {return y[k]})",
-              ".colour(function(k) {return k == selPoint ? 'red' : 'black';})",
-              ".on_click(function(k) {selPoint = k; ws.send('selPoint <<- ' + k); ws.send('print(selPoint)'); sc.update()})", 
-              ".place()", collapse = "\n"))
-
-closePage()
-
-openPage(localDirectory = "test/linkedCharts")
-
