7c7970dd8262a72925cf9917e8ebfe4dcfd839c7
Svetlana <s.ovchinnikova@zmbh.uni-heidelberg.de>
2019-12-04 14:17:49 +0100

    sendCommand unit test updated

--- tests/testthat/test_sendCommand.R
+++ tests/testthat/test_sendCommand.R
@@ -6,27 +6,26 @@ test_that("Commands can be send, received and stored", {
 
   app$openPage()
   
-  session <- app$getSession(app$getSessionIds()$id)
+  session <- app$getSession()
   
   session$sendCommand(paste0("jrc.sendCommand('message(\"Hi\")')"), wait = 3)
-  expect_equal(length(session$getMessageIds()), 1)
+  expect_length(session$getMessageIds(), 1)
   session$sendCommand(paste0("jrc.sendCommand('message(\"Hi\")')"), wait = 3)
   
   messageIds <- session$getMessageIds()
-  expect_equal(length(messageIds), 2)
+  expect_length(messageIds, 2)
   
   expect_message(session$authorize(messageIds[1]), "Hi")
 
-  expect_equal(length(session$getMessageIds()), 1)
+  expect_length(session$getMessageIds(), 1)
   session$removeMessage(messageIds[2])
-  expect_equal(length(session$getMessageIds()), 0)
+  expect_length(session$getMessageIds(), 0)
   
   k_send <- sample(1000, 1)
   k_received <- -1
   
   session$sendCommand(paste0("jrc.sendCommand('k_received <<- ", k_send, "')"), wait = 3)
-  messageId <- session$getMessageIds()
-  session$authorize(messageId)   
+  session$authorize()   
   
   expect_equal(k_received, k_send)  
   
@@ -42,7 +41,7 @@ test_that("Commands can be evaluated in the given environment", {
   
   app$openPage()
   
-  session <- app$getSession(app$getSessionIds()$id)
+  session <- app$getSession()
   
   e$k_send <- sample(1000, 1)
   e$k_received <- -1
@@ -63,9 +62,8 @@ test_that("Wrapper function is working", {
   k_received <- -1
   
   sendCommand(paste0("jrc.sendCommand('k_received <<- ", k_send, "')"), wait = 3)
-  app <- getPage()
-  session <- app$getSession(app$getSessionIds()$id)
-  messageId <- session$getMessageIds()
+  session <- getSession()
+  messageId <- getMessageIds()
 
   authorize(session$id, messageId)
   
